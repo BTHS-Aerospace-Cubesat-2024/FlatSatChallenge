a00bb31b5f6597b7ec23fd16195127d5f0439f11
Samuel Ashkenas <72979934+SamAsh11414@users.noreply.github.com>
2024-02-06 12:14:48 -0500

    picam2 commented

--- Python/FlatSat_student.py
+++ Python/FlatSat_student.py
@@ -81,7 +81,7 @@ def take_photo():
                 time.sleep(1)
                 camera_config = picam2.create_preview_configuration()
                 picam2.configure(camera_config)
-                picam2.start_preview(Preview.DRM)
+                #picam2.start_preview(Preview.DRM)
                 picam2.start()
                 time.sleep(2)
                 name = img_gen("SamA")
